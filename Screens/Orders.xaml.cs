ae2f79e103ead53e0b0d17875384e68ef9cda10f
RFENZL <92161747+RFENZL@users.noreply.github.com>
2024-05-24 16:58:30 +0200

    commit

--- Screens/Orders.xaml.cs
+++ Screens/Orders.xaml.cs
@@ -1,27 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace orderApp.Screens
 {
-    /// <summary>
-    /// Logique d'interaction pour Orders.xaml
-    /// </summary>
     public partial class Orders : Window
     {
+        private string connectionString = "Server=.\\SQLEXPRESS;Database=orderAppBDD;Trusted_Connection=True;";
         public Orders()
         {
             InitializeComponent();
+            ListOrders();
         }
 
         public void Back_Click(object sender, RoutedEventArgs e)
@@ -30,5 +22,87 @@ namespace orderApp.Screens
             mainWindow.Show();
             this.Close();
         }
+
+        public void ListOrders()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT Id, Name, Price FROM orders", connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        var ordersDict = new Dictionary<int, List<string>>();
+
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+
+                            if (!ordersDict.ContainsKey(id))
+                            {
+                                ordersDict[id] = new List<string>();
+                            }
+                            ordersDict[id].Add(name);
+                        }
+
+                        foreach (var kvp in ordersDict)
+                        {
+                            int id = kvp.Key;
+                            List<string> names = kvp.Value;
+
+                            StackPanel itemPanel = new StackPanel
+                            {
+                                Orientation = Orientation.Horizontal,
+                                Margin = new Thickness(0, 5, 0, 5)
+                            };
+
+                            Label label = new Label
+                            {
+                                Content = $"ID: {id}",
+                                Width = 100,
+                                VerticalAlignment = VerticalAlignment.Center,
+                                Foreground = System.Windows.Media.Brushes.White
+                            };
+                            itemPanel.Children.Add(label);
+
+                            ComboBox comboBox = new ComboBox
+                            {
+                                Width = 150,
+                                Margin = new Thickness(10, 0, 0, 0)
+                            };
+
+                            foreach (var name in names)
+                            {
+                                comboBox.Items.Add(name);
+                            }
+
+                            if (comboBox.Items.Count > 0)
+                            {
+                                comboBox.SelectedIndex = 0;
+                            }
+
+                            itemPanel.Children.Add(comboBox);
+                            ItemsPanel.Children.Add(itemPanel);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No rows found.");
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+    }
+
+    public class OrdersList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
     }
 }
--- Screens/ProductList.xaml.cs
+++ Screens/ProductList.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using orderApp.Class;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace orderApp.Screens
 {
@@ -9,9 +12,12 @@ namespace orderApp.Screens
     /// </summary>
     public partial class ProductList : Window
     {
+        private string connectionString = "Server=.\\SQLEXPRESS;Database=orderAppBDD;Trusted_Connection=True;";
+        List<Product> products = new List<Product>();
         public ProductList()
         {
             InitializeComponent();
+            ListProduct();
         }
 
         public void Back_Click(object sender, RoutedEventArgs e)
@@ -21,5 +27,73 @@ namespace orderApp.Screens
             this.Close();
         }
 
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            int productId = (int)button.Tag;
+            InfoProduct infoproduct = new InfoProduct(productId);
+            infoproduct.Show();
+            this.Close();
+        }
+
+        public void ListProduct()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT id, name FROM product", connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        var productList = new List<ProductItem>(); // Liste pour stocker les produits
+
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+                            Console.WriteLine(id.ToString(), name);
+                            var product = new ProductItem
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                               
+                            };
+
+                            productList.Add(product);
+                        }
+
+                        // Liaison de la liste de produits à votre interface utilisateur
+                        ProductListItems.ItemsSource = productList;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No rows found.");
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+        private void ProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            int productId = (int)button.Tag;
+
+           
+        }
+
+
+
+
+
     }
+}
+public class ProductItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
 }
